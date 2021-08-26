9cb1bb73ab0c3e8763835e929e2668cde16d8b3c
Maliski <86474495+Maliski@users.noreply.github.com>
2021-08-26 19:51:25 +0000

    all

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
 #Data Types
+
+#String
+
+#important to remember programmers always start with 0 because we work with binary
+#H=0 e=1 l=2 l=3 l=4
+print("Hello"[4])
+
+"123"
+#if you printed "123+456" it wouldnt work as a sum the computer reads it as a String
+print("123"+"345")
+
+#integer = programming lingo for whole numbers, numbers without decimal places.
+
+print ( 123 + 345) #makes a calculation because it is not trying to read a string. 
+
+123_456_789 #interger's replace comma's for large intergers with underscores. Computer will ignore it, the underscore benefits us to read it.
+
+#Float....floating point number (when you include a decimal)
+
+3141.59
+
+#Boolean - True or False, always begin with caps for these 
+
+True
+False
